ceba7bd4c754857bef6d62f3926e2e8b31c85cdb
Yannick Gibson <yannick.gibson@seznam.cz>
2022-06-17 14:50:50 +0200

    first commit - hello world

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, send_from_directory
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
